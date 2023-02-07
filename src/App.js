a9769c09f95cad47401eb86eabcf9e4aaa46c80f
HariRithanya <haririthanya.m@thoughtworks.com>
2023-02-07 11:56:06 +0530

    Add. Select a slot

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' 
+import interactionPlugin from "@fullcalendar/interaction";
+
 import './App.css';
 
 function App() {
+  const [events, setEvents] = useState([]);
+
+  function handleSelectSlot(slotInfo){
+    alert(JSON.stringify(slotInfo))
+  }
   return (
     <FullCalendar
+      selectable
       headerToolbar={{
         start: "today prev next",
         end: "dayGridMonth dayGridWeek dayGridDay",
       }}
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
+      plugins={[ dayGridPlugin, interactionPlugin ]}
+      initialView="dayGridWeek"
+      events={events}
       views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+      select={handleSelectSlot}
     />
   );
 }
