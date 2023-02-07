d0a5d886de2ad470deaa7c2b36f245efa0cef84c
HariRithanya <haririthanya.m@thoughtworks.com>
2023-02-07 21:33:43 +0530

    Add. Unselect slot

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ import './App.css';
 function App() {
   const [events, setEvents] = useState([{title: 'From Calendar', start: '2023-02-08T12:30:00', end:'2023-02-08T12:45:00'}]);
   const [reservedSlots, setReservedSlots] = useState([]);
+  const idTemplate = `id-`
+  const [id, setId] = useState(0);
 
   function handleSelectSlot(slotInfo){
     const newEvent = {
+      id: `idTemplate-${id}`,
       title: `${slotInfo.start} - ${slotInfo.end}`, 
       start: slotInfo.start, 
       end: slotInfo.end
     }
-    setReservedSlots((prevEvents) => ([...prevEvents, newEvent]));
+    setId((prevId) => (prevId + 1))
+    setReservedSlots((prevEvents) => ([...prevEvents,{...slotInfo, id: `idTemplate-${id}`}]));
     setEvents((prevEvents) => ([...prevEvents, newEvent]))
+  }
+
+  function handleUnSelectSlot(slotInfo) {
+    const chosenEvent = reservedSlots.findIndex(slot => slot.id === slotInfo.event.id)
+    const selectedEvent = events.findIndex(slot => slot.id === slotInfo.event.id)
+    chosenEvent !== -1 && setReservedSlots(reservedSlots.splice(chosenEvent, 1));
+    selectedEvent !== -1 && setEvents(events.splice(selectedEvent, selectedEvent+1));
+    if(reservedSlots.indexOf(slotInfo.event.id) > 0) alert('yes')
   }
 
   return (
@@ -34,6 +46,7 @@ function App() {
       slotLabelInterval={"01:00"}
       slotDuration={'00:30:00'}
       nowIndicator
+      eventClick={handleUnSelectSlot}
     />
   );
 }
